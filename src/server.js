b9c39a7e6fd7d8fbec64487f1e5e991e0d51de29
Valéria Cavania <98471520+ValeriaCavania@users.noreply.github.com>
2025-07-08 16:21:03 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,10 +18,7 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do AllBooks")
-  console.log("API disponível através da url http://localhost:8000") 
-=======
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 8d659f2bccc91da4f83faaabd1d8e1751fa4ee49
+console.log("Boas-vindas ao AllBooks")
+console.log("Essa é uma API de uma loja de livros online")
+console.log("Acesse essa API em http://localhost:8000")
 })
